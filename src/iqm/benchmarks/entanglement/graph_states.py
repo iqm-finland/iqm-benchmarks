bf016f42bac0533ad8a702204b1e194b74fb7545
Pedro Figueroa <pedro.romero@meetiqm.com>
2024-12-18 16:06:57 +0100

    WIP - small changes

--- src/iqm/benchmarks/entanglement/graph_states.py
+++ src/iqm/benchmarks/entanglement/graph_states.py
@@ -15,7 +15,7 @@
 """
 Graph states benchmark
 """
-
+from time import strftime
 from typing import Sequence, Type
 
 from qiskit import QuantumCircuit, transpile
@@ -23,7 +23,8 @@ import xarray as xr
 
 from iqm.benchmarks import Benchmark
 from iqm.benchmarks.benchmark import BenchmarkConfigurationBase
-from iqm.benchmarks.utils import generate_minimal_edge_layers, set_coupling_map
+from iqm.benchmarks.logging_config import qcvv_logger
+from iqm.benchmarks.utils import generate_minimal_edge_layers, set_coupling_map, timeit
 from iqm.qiskit_iqm.iqm_backend import IQMBackendBase
 
 
@@ -57,27 +58,85 @@ class GraphStatesBenchmark(Benchmark):
     # analysis_function = staticmethod(negativity_analysis)
     name = "graph_states"
 
-    def __init__(self, backend: IQMBackendBase, configuration: "GraphStatesConfiguration"):
-        """Construct the GHZBenchmark class.
+    def __init__(self, backend_arg: IQMBackendBase, configuration: "GraphStatesConfiguration"):
+        """Construct the GraphStatesBenchmark class.
 
         Args:
-            backend (IQMBackendBase): the backend to execute the benchmark on
+            backend_arg (IQMBackendBase): the backend to execute the benchmark on
             configuration (QuantumVolumeConfiguration): the configuration of the benchmark
         """
-        super().__init__(backend, configuration)
+        super().__init__(backend_arg, configuration)
+
+        self.backend_configuration_name = backend_arg if isinstance(backend_arg, str) else backend_arg.name
+
+        self.qubits = configuration.qubits
+
+        self.session_timestamp = strftime("%Y%m%d-%H%M%S")
+        self.execution_timestamp = ""
+
+
+    def add_all_meta_to_dataset(self, dataset: xr.Dataset):
+        """Adds all configuration metadata and circuits to the dataset variable
+
+        Args:
+            dataset (xr.Dataset): The xarray dataset
+        """
+        dataset.attrs["session_timestamp"] = self.session_timestamp
+        dataset.attrs["execution_timestamp"] = self.execution_timestamp
+        dataset.attrs["backend_configuration_name"] = self.backend_configuration_name
+        dataset.attrs["backend_name"] = self.backend.name
+
+        for key, value in self.configuration:
+            if key == "benchmark":  # Avoid saving the class object
+                dataset.attrs[key] = value.name
+            else:
+                dataset.attrs[key] = value
+        # Defined outside configuration - if any
+
+
+    @timeit
+    def add_all_circuits_to_dataset(self, dataset: xr.Dataset):
+        """Adds all generated circuits during execution to the dataset variable
+
+        Args:
+            dataset (xr.Dataset):  The xarray dataset
+
+        Returns:
+
+        """
+        qcvv_logger.info(f"Adding all circuits to the dataset")
+        for key, circuit in zip(
+            ["transpiled_circuits", "untranspiled_circuits"], [self.transpiled_circuits, self.untranspiled_circuits]
+        ):
+            dictionary = {}
+            for outer_key, outer_value in circuit.items():
+                dictionary[str(outer_key)] = {
+                    str(inner_key): inner_values for inner_key, inner_values in outer_value.items()
+                }
+            dataset.attrs[key] = dictionary
+
 
     def execute(self, backend) -> xr.Dataset:
         """
         Executes the benchmark.
         """
+        self.execution_timestamp = strftime("%Y%m%d-%H%M%S")
+
         dataset = xr.Dataset()
+        self.add_all_meta_to_dataset(dataset)
+
+        graph_state = generate_graph_state(self.qubits, backend)
+
         return dataset
 
 
 class GraphStatesConfiguration(BenchmarkConfigurationBase):
-    """Graph States Benchmark configuration
+    """Graph States Benchmark configuration.
 
     Attributes:
+        benchmark (Type[Benchmark]): GraphStatesBenchmark
+        qubits (Sequence[int]): The physical qubit layout in which to benchmark graph state generation.
     """
 
     benchmark: Type[Benchmark] = GraphStatesBenchmark
+    qubits: Sequence[int]
