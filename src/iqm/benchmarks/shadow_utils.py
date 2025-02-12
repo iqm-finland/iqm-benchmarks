7164bf8ed445df5ebf6172843e5e83686c12e394
Pedro Figueroa <pedro.romero@meetiqm.com>
2025-02-12 10:52:16 +0100

    Modified (local) shadow tomography function to take either Haar or Clifford

--- src/iqm/benchmarks/shadow_utils.py
+++ src/iqm/benchmarks/shadow_utils.py
@@ -1,5 +1,5 @@
 import random
-from typing import List, Optional, Sequence, Tuple
+from typing import List, Optional, Sequence, Tuple, Dict, Literal, cast
 
 import numpy as np
 from qiskit import ClassicalRegister, QuantumCircuit
@@ -33,36 +33,67 @@ def CUE(random_gen, nh):
 
 
 @timeit
-def haar_shadow_tomography(
+def local_shadow_tomography(
     qc: QuantumCircuit,
     Nu: int,
     active_qubits: Sequence[int],
     measure_other: Optional[Sequence[int]] = None,
     measure_other_name: Optional[str] = None,
+    clifford_or_haar: Literal["clifford", "haar"] = "clifford",
+    cliffords_1q: Optional[Dict[str, QuantumCircuit]] = None,
 ) -> Tuple[List[np.ndarray], List[QuantumCircuit]]:
     """Prepares the circuits to perform Haar shadow tomography.
 
     Args:
         qc (QuantumCircuit): The quantum circuit to which random unitaries are appended.
         Nu (Int): Number of local random unitaries used.
-        active_qubits (Sequence[int]): List of active qubits.
-        measure_other (Optional[Sequence[int]]): Whether to measure other qubits in qc.
+        active_qubits (Sequence[int]): The Sequence of active qubits.
+        measure_other (Optional[Sequence[int]]): Whether to measure other qubits in the qc QuantumCircuit.
                 * Default is None.
         measure_other_name (Optional[str]): Name of the classical register to assign measure_other.
+        clifford_or_haar (Literal["clifford", "haar"]): Whether to use Clifford or Haar random 1Q gates.
+                * Default is "clifford".
+        cliffords_1q (Optional[Dict[str, QuantumCircuit]]): dictionary of 1-qubit Cliffords in terms of IQM-native r and CZ gates
+                * Default is None.
+
+    Raises:
+        ValueError: If clifford_or_haar is not "clifford" or "haar".
+        Exception: If cliffords_1q is None and clifford_or_haar is "clifford".
+
     Returns:
         Tuple(List[ndarray], List[QuantumCircuit])
-        - List[ndarray]: List of unitary gates for each random initialisation and qubit.
+        - List[ndarray] | List[str]: Either:
+                * List of unitary gates (numpy ndarray) for each random initialisation and qubit, if clifford_or_haar == 'haar'.
+                * List of (str) Clifford labels, if clifford_or_haar == 'clifford'.
         - List[QuantumCircuit]: List of tomography circuits.
     """
-    qclist = []
-    unitaries = np.zeros((Nu, len(active_qubits), 2, 2), dtype=np.complex_)
+    if clifford_or_haar not in ["clifford", "haar"]:
+        raise ValueError("clifford_or_haar must be either 'clifford' or 'haar'.")
+    elif clifford_or_haar == "clifford" and cliffords_1q is None:
+        raise Exception("cliffords_1q dictionary must be provided if clifford_or_haar is 'clifford'.")
+    elif clifford_or_haar == "clifford":
+        # Get the keys of the Clifford dictionaries
+        clifford_1q_keys = list(cliffords_1q.keys())
 
-    for iu in range(Nu):
+    qclist = []
+
+    if clifford_or_haar == "haar":
+        unitaries = np.zeros((Nu, len(active_qubits), 2, 2), dtype=np.complex_)
+    else:
+        unitaries = []
+
+    for u in range(Nu):
         qc_copy = qc.copy()
-        for idx, z in enumerate(active_qubits):
-            temp_U = CUE(random_gen, 2)
-            qc_copy.append(UnitaryGate(temp_U), [z])
-            unitaries[iu, idx, :, :] = np.array(temp_U)
+        for q_idx, qubit in enumerate(active_qubits):
+            if clifford_or_haar == "haar":
+                temp_U = CUE(random_gen, 2)
+                qc_copy.append(UnitaryGate(temp_U), [qubit])
+                unitaries[u, q_idx, :, :] = np.array(temp_U)
+            elif clifford_or_haar == "clifford":
+                rand_key = random.choice(clifford_1q_keys)
+                c_1q = cast(dict, cliffords_1q)[rand_key]
+                qc_copy.append(c_1q, [qubit])
+                unitaries.append(rand_key)
 
         qc_copy.barrier()
 
@@ -82,13 +113,13 @@ def haar_shadow_tomography(
     return unitaries, qclist
 
 
-def get_shadow(counts, Unitary, subsystem):
+def get_shadow(counts: Dict[str, int], Unitary: np.ndarray, subsystem: Sequence[int]):
     """Constructs shadows for each individual initialisation.
 
     Args:
-        counts (Dict): bit-string counts.
-        Unitary (Array): local random unitaries used for a given initialisation.
-        subsystem (List[int]): List of qubits to construct the shadow of.
+        counts (Dict[str, int]): a dictionary of bit-string counts.
+        Unitary (Numpy Array): local random unitaries used for a given initialisation.
+        subsystem (Sequence[int]): Sequence of qubits to construct the shadow of.
     Returns:
         rhoshadows (Array): shadow of considered subsystem.
     """
@@ -98,18 +129,18 @@ def get_shadow(counts, Unitary, subsystem):
     proj[0, :, :] = np.array([[1, 0], [0, 0]])
     proj[1, :, :] = np.array([[0, 0], [0, 1]])
     shots = sum(list(counts.values()))
-    for ist in counts.keys():
-        rhoj = 1
+    for bit_strings in counts.keys():
+        rho_j = 1
         for j in subsystem:
-            sj = int(ist[::-1][j])
-            rhoj = np.kron(
-                rhoj,
+            s_j = int(bit_strings[::-1][j])
+            rho_j = np.kron(
+                rho_j,
                 3
-                * np.einsum('ab,bc,cd', np.transpose(np.conjugate(Unitary[j, :, :])), proj[sj, :, :], Unitary[j, :, :])
+                * np.einsum('ab,bc,cd', np.transpose(np.conjugate(Unitary[j, :, :])), proj[s_j, :, :], Unitary[j, :, :])
                 - np.array([[1, 0], [0, 1]]),
             )
 
-        rhoshadows += counts[ist] * rhoj / shots
+        rhoshadows += rho_j * counts[bit_strings] / shots
 
     return rhoshadows
 
