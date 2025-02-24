Fixed bug in mGST gradient computation for POVM elements

src/iqm/benchmarks/utils.py
@@ -32,7 +32,7 @@ from qiskit import ClassicalRegister, transpile
 from qiskit.converters import circuit_to_dag
 from qiskit.transpiler import CouplingMap
 import requests
-import rustworkx as rx
+from rustworkx import PyGraph, visualization, spring_layout
 import xarray as xr
 
 from iqm.benchmarks.logging_config import qcvv_logger
@@ -579,7 +579,7 @@ class GraphPositions:
     }
 
     @staticmethod
-    def create_positions(graph: rx.PyGraph, topology: Literal["star", "crystal"]) -> Dict[int, Tuple[float, float]]:
+    def create_positions(graph: PyGraph, topology: Literal["star", "crystal"]) -> Dict[int, Tuple[float, float]]:
         """Generate node positions for a given graph and topology.
 
         Args:
@@ -611,7 +611,7 @@ class GraphPositions:
             fixed_pos = {0: (1.0, 1.0)}  # For more consistent layouts
 
             # Get spring layout with one fixed position
-            pos = rx.spring_layout(graph, scale=2, pos=fixed_pos, num_iter=300, fixed={0})  # keep node 0 fixed
+            pos = spring_layout(graph, scale=2, pos=fixed_pos, num_iter=300, fixed={0})  # keep node 0 fixed
 
         return pos
 
@@ -680,7 +680,7 @@ def plot_layout_fidelity_graph(cal_url: str, qubit_layouts: list[list[int]], sta
     weights = -np.log(np.array(fidelities_cal))
     edges_graph = [tuple(edge) + (weight,) for edge, weight in zip(edges_cal, weights)]
 
-    graph = rx.PyGraph()
+    graph = PyGraph()
 
     # Add nodes
     nodes = set()
@@ -698,7 +698,7 @@ def plot_layout_fidelity_graph(cal_url: str, qubit_layouts: list[list[int]], sta
         pos = GraphPositions.create_positions(graph, topology)
 
     # Define node colors
-    node_colors = ['lightgrey' for _ in range(len(nodes))]
+    node_colors = ["lightgrey" for _ in range(len(nodes))]
     for qb in set([qb for layout in qubit_layouts for qb in layout]):
         node_colors[qb] = "orange"
 
@@ -719,7 +719,7 @@ def plot_layout_fidelity_graph(cal_url: str, qubit_layouts: list[list[int]], sta
     fig, _ = plt.subplots(figsize=(6, 6))
 
     # Draw the graph
-    rx.visualization.mpl_draw(
+    visualization.mpl_draw(
         graph,
         with_labels=True,
         node_color=node_colors,
@@ -735,13 +735,13 @@ def plot_layout_fidelity_graph(cal_url: str, qubit_layouts: list[list[int]], sta
         x = (x1 + x2) / 2
         y = (y1 + y2) / 2
         plt.annotate(
-            f'{edge[2]:.1e}',
+            f"{edge[2]:.1e}",
             xy=(x, y),
             xytext=(0, 0),
-            textcoords='offset points',
-            ha='center',
-            va='center',
-            bbox=dict(boxstyle='round,pad=0.2', fc='white', ec='none', alpha=0.6),
+            textcoords="offset points",
+            ha="center",
+            va="center",
+            bbox=dict(boxstyle="round,pad=0.2", fc="white", ec="none", alpha=0.6),
         )
 
     plt.gca().invert_yaxis()

src/mGST/low_level_jit.py
@@ -299,7 +299,7 @@ def dK(X, K, E, rho, J, y, d, r, rK):
     return dK_.reshape(d, rK, pdim, pdim) * 2 / m / n_povm
 
 
-@njit(cache=True)
+@njit(cache=True, parallel=False)
 def dK_dMdM(X, K, E, rho, J, y, d, r, rK):
     """Compute the derivatives of the objective function with respect to K and the
     product of derivatives of the measurement map with respect to K.
@@ -403,7 +403,7 @@ def ddM(X, K, E, rho, J, y, d, r, rK):
     ddK = np.zeros((d**2, rK**2, r, r))
     ddK = np.ascontiguousarray(ddK.astype(np.complex128))
     dconjdK = np.zeros((d**2, rK**2, r, r))
-    dconjdK = np.ascontiguousarray(ddK.astype(np.complex128))
+    dconjdK = np.ascontiguousarray(dconjdK.astype(np.complex128))
     m = len(J)
     for k in range(d**2):
         k1, k2 = local_basis(k, d, 2)
@@ -522,16 +522,17 @@ def dA(X, A, B, J, y, r, pdim, n_povm):
     for k in range(n_povm):
         E[k] = (A[k].T.conj() @ A[k]).reshape(-1)
     rho = (B @ B.T.conj()).reshape(-1)
-    dA_ = np.zeros((n_povm, pdim, pdim))
-    dA_ = dA_.astype(np.complex128)
+    dA_ = np.zeros((n_povm, pdim, pdim)).astype(np.complex128)
     m = len(J)
     for n in prange(m):  # pylint: disable=not-an-iterable
-        jE = J[n][J[n] >= 0][0]
-        j = J[n][J[n] >= 0][1:]
+        j = J[n][J[n] >= 0]
         inner_deriv = contract(X, j) @ rho
-        D_ind = E[jE].conj().dot(inner_deriv) - y[n]
-        dA_[jE] += D_ind * A[jE] @ inner_deriv.reshape(pdim, pdim).T.conj()
-    return dA_
+        dA_step = np.zeros((n_povm, pdim, pdim)).astype(np.complex128)
+        for o in range(n_povm):
+            D_ind = E[o].conj()@inner_deriv - y[o,n]
+            dA_step[o] += D_ind * A[o].conj() @ inner_deriv.reshape(pdim, pdim).T
+        dA_ += dA_step
+    return dA_ * 2 / m / n_povm
 
 
 @njit(parallel=True, cache=True)
@@ -621,13 +622,21 @@ def ddA_derivs(X, A, B, J, y, r, pdim, n_povm):
     for n in prange(m):  # pylint: disable=not-an-iterable
         j = J[n][J[n] >= 0]
         R = contract(X, j) @ rho
+        dA_step = np.zeros((n_povm, pdim, pdim)).astype(np.complex128)
+        dMdM_step = np.zeros((n_povm, r, r)).astype(np.complex128)
+        dMconjdM_step = np.zeros((n_povm, r, r)).astype(np.complex128)
+        dconjdA_step = np.zeros((n_povm, r, r)).astype(np.complex128)
         for o in range(n_povm):
             D_ind = E[o].conj() @ R - y[o, n]
             dM = A[o].conj() @ R.reshape(pdim, pdim).T
-            dMdM[o] += np.outer(dM, dM)
-            dMconjdM[o] += np.outer(dM.conj(), dM)
-            dA_[o] += D_ind * dM
-            dconjdA[o] += D_ind * np.kron(np.eye(pdim).astype(np.complex128), R.reshape(pdim, pdim).T)
+            dMdM_step[o] += np.outer(dM, dM)
+            dMconjdM_step[o] += np.outer(dM.conj(), dM)
+            dA_step[o] += D_ind * dM
+            dconjdA_step[o] += D_ind * np.kron(np.eye(pdim).astype(np.complex128), R.reshape(pdim, pdim).T)
+        dA_ += dA_step
+        dMdM += dMdM_step
+        dMconjdM += dMconjdM_step
+        dconjdA += dconjdA_step
     return dA_ * 2 / m / n_povm, dMdM * 2 / m / n_povm, dMconjdM * 2 / m / n_povm, dconjdA * 2 / m / n_povm
 
 