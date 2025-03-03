8ce1ad3c25fccca30ec8fa4e922474027fcac13d
Raphael <53472238+rabrie@users.noreply.github.com>
2025-03-03 16:08:54 +0100

    Update src/iqm/benchmarks/entanglement/ghz.py
    Co-authored-by: Pedro Figueroa-Romero <146174752+pedrofigro@users.noreply.github.com>

--- src/iqm/benchmarks/entanglement/ghz.py
+++ src/iqm/benchmarks/entanglement/ghz.py
@@ -99,7 +99,6 @@ def fidelity_ghz_randomized_measurements(
                 exponent = hamming(list(sa), list(sb)) * num_qubits
                 p_sum.append(np.power(-2, -exponent) * probabilities_sample[sa] * ideal_probabilities[u][sb])
         fid_rm.append((2**num_qubits) * sum(p_sum))
-        fid_rm.append((2**num_qubits) * sum(p_sum))
     values = {"fidelity": np.mean(fid_rm)}
     uncertainties = {"fidelity": np.std(fid_rm) / np.sqrt(num_rms)}
 
