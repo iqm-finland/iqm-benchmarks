56570f7f86224a2dd30739c88f9c72c26058efd7
aniket.rath <aniket.rath@meetiqm.com>
2025-11-03 14:52:08 +0100

    isort and black

--- src/iqm/benchmarks/optimization/qscore.py
+++ src/iqm/benchmarks/optimization/qscore.py
@@ -12,13 +12,10 @@ import matplotlib.pyplot as plt
 from networkx import Graph
 import networkx as nx
 import numpy as np
-from qiskit import QuantumCircuit
+from qiskit import ClassicalRegister, QuantumCircuit, QuantumRegister, transpile
+from qiskit.circuit.library import RZZGate
 from scipy.optimize import basinhopping, minimize
 import xarray as xr
-from qiskit.circuit.library import RZZGate
-from iqm.qiskit_iqm import IQMCircuit, transpile_to_IQM
-from qiskit import QuantumRegister, ClassicalRegister, transpile
-from iqm.iqm_client.transpile import ExistingMoveHandlingOptions
 
 from iqm.benchmarks.benchmark import BenchmarkConfigurationBase
 from iqm.benchmarks.benchmark_definition import (
@@ -38,6 +35,8 @@ from iqm.benchmarks.utils import (  # execute_with_dd,
     submit_execute,
     xrvariable_to_counts,
 )
+from iqm.iqm_client.transpile import ExistingMoveHandlingOptions
+from iqm.qiskit_iqm import IQMCircuit, transpile_to_IQM
 from iqm.qiskit_iqm.iqm_backend import IQMBackendBase
 
 
@@ -326,6 +325,7 @@ def get_optimal_angles(num_layers: int) -> List[float]:
 
     return OPTIMAL_INITIAL_ANGLES[str(num_layers)]
 
+
 def group_rzz_gates(circuit: QuantumCircuit):
     """
     Extract and group commuting RZZ gates so that gates sharing a qubit
@@ -394,6 +394,7 @@ def group_rzz_gates(circuit: QuantumCircuit):
         B.remove_node(hub)
 
     return grouped_rzz
+
 
 def plot_approximation_ratios(
     nodes: list[int],
@@ -698,10 +699,10 @@ class QScoreBenchmark(Benchmark):
     def greedy_vertex_cover_with_mapping(self, G: nx.Graph):
         """
         Approximate minimum vertex cover with mapping of nodes -> edges they cover.
-        
+
         Args:
             G (nx.Graph): input graph
-        
+
         Returns:
             cover_map (dict): {node: [(u,v), ...]} showing which edges each node covers
         """
@@ -759,7 +760,7 @@ class QScoreBenchmark(Benchmark):
         compr = QuantumRegister(1, 'compr')
         q = QuantumRegister(num_qubits, 'q')
         c = ClassicalRegister(num_qubits, 'c')
-        qaoa_qc = IQMCircuit(compr, q, c) # num_qb+1,num_qb)
+        qaoa_qc = IQMCircuit(compr, q, c)  # num_qb+1,num_qb)
         # in case the graph is trivial: return empty circuit
         if num_qubits == 0:
             return QuantumCircuit(1)
@@ -767,10 +768,10 @@ class QScoreBenchmark(Benchmark):
             qaoa_qc.h(i)
         for layer in range(self.num_qaoa_layers):
             for move_qubit in covermap.keys():
-                qaoa_qc.move(move_qubit+1, 0)
+                qaoa_qc.move(move_qubit + 1, 0)
                 for edge_qubit in covermap[move_qubit]:
-                    qaoa_qc.rzz(2 * gamma[layer], 0, edge_qubit+1)
-                qaoa_qc.move(move_qubit+1, 0)
+                    qaoa_qc.rzz(2 * gamma[layer], 0, edge_qubit + 1)
+                qaoa_qc.move(move_qubit + 1, 0)
 
             # include edges of the virtual node as rz terms
             for vn in self.virtual_nodes:
@@ -782,20 +783,20 @@ class QScoreBenchmark(Benchmark):
                         sign = 1.0
                         if vn[1] == 1:
                             sign = -1.0
-                        qaoa_qc.rz(sign * 2.0 * gamma[layer], self.node_to_qubit[edge[1]]+1)
+                        qaoa_qc.rz(sign * 2.0 * gamma[layer], self.node_to_qubit[edge[1]] + 1)
 
-            for i in range(1, num_qubits+1):
+            for i in range(1, num_qubits + 1):
                 qaoa_qc.rx(2 * beta[layer], i)
         qaoa_qc.barrier()
-        qaoa_qc.measure(q,c)
+        qaoa_qc.measure(q, c)
 
         return qaoa_qc
-    
+
     def generate_maxcut_ansatz(  # pylint: disable=too-many-branches
         self,
         graph: Graph,
         theta: list[float],
-        rzz_list = None,
+        rzz_list=None,
     ) -> QuantumCircuit:
         """Generate an ansatz circuit for QAOA MaxCut, with measurements at the end.
 
@@ -828,7 +829,7 @@ class QScoreBenchmark(Benchmark):
         for layer in range(self.num_qaoa_layers):
             if rzz_list is not None and layer == 0:
                 for rzzs in rzz_list:
-                    qaoa_qc.rzz(2 * gamma[layer],rzzs[0], rzzs[1])
+                    qaoa_qc.rzz(2 * gamma[layer], rzzs[0], rzzs[1])
             else:
                 for edge in self.graph_physical.edges():
                     i = self.node_to_qubit[edge[0]]
@@ -1065,9 +1066,16 @@ class QScoreBenchmark(Benchmark):
                 seed += 1
                 qcvv_logger.debug(f"Solved the MaxCut on graph {instance+1}/{self.num_instances}.")
 
-
             if self.backend.has_resonators():
-                transpiled_qc = [transpile_to_IQM(qc, self.backend, optimize_single_qubits=self.optimize_sqg, existing_moves_handling=ExistingMoveHandlingOptions.KEEP) for qc in qc_list]
+                transpiled_qc = [
+                    transpile_to_IQM(
+                        qc,
+                        self.backend,
+                        optimize_single_qubits=self.optimize_sqg,
+                        existing_moves_handling=ExistingMoveHandlingOptions.KEEP,
+                    )
+                    for qc in qc_list
+                ]
             else:
                 transpiled_qc, _ = perform_backend_transpilation(qc_list, **transpilation_params)
 
@@ -1089,9 +1097,7 @@ class QScoreBenchmark(Benchmark):
             num_instances_with_edges = len(instance_with_edges)
             if self.REM:
                 counts_retrieved, time_retrieve = retrieve_all_counts(jobs)
-                rem_counts = apply_readout_error_mitigation(
-                    backend, transpiled_qc, counts_retrieved, self.mit_shots
-                )
+                rem_counts = apply_readout_error_mitigation(backend, transpiled_qc, counts_retrieved, self.mit_shots)
                 execution_results.extend(
                     rem_counts[0][instance].nearest_probability_distribution()
                     for instance in range(num_instances_with_edges)
